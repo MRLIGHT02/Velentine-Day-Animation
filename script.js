affe664f2582058d4be50dfdb77bd0feaca09cfc
Saif Ali <117639348+MRLIGHT02@users.noreply.github.com>
2025-02-06 23:46:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function sendMail() {
+    let inputs = {
+        name: document.getElementById("name").value,
+        // let email : document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        phone_id: document.getElementById("phone_id").value,
+        user_id: document.getElementById("user_id").value,
+        reply_to: "none",
+    }
+    emailjs
+        .send("service_q5zl7b2", "template_gtrk0rt", inputs);
+}

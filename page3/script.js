14169a7466e2f3c26f2933d146d11f7c29adad87
Saif Ali <117639348+MRLIGHT02@users.noreply.github.com>
2025-02-12 06:09:56 +0000

    Update script.js

--- page3/script.js
+++ page3/script.js
@@ -6,7 +6,12 @@ var isNotClicked=true;
  var loveCalculator=function () {
   var yourName = document.getElementById("firstname").value;
   var loversName = document.getElementById("lovername").value;
-  var loveScore = Math.random() * 100;
+  // var loveScore = Math.random() * 100;
+ // adding function to get score between 80 to 100 
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+  var loveScore = getRandomNumber(80,100)
   var loveFinal = Math.round(loveScore);
   if (yourName === "") {
     alert("please enter your name");
